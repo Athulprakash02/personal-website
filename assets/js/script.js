6a86f28625a47aa4b15f1f8da43660a1e8c335e1
Athulprakash02 <athulkichu02@gmail.com>
2023-01-02 21:00:07 +0530

    done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,17 @@
-// function validateForm() {
-//     const NAMEValue = document.getElementById('NAME').value.trim();
-//     const eMAILValue = document.getElementById('eMAIL').value.trim();
-//     const MESSAGEValue = document.getElementById('MESSAGE').value.trim();
-//     const PHONEValue = document.getElementById('PHONE').value.trim();
-
-//     if (NAMEValue === '' || eMAILValue === '' || MESSAGEValue === '' || PHONEValue === '') {
-//         alert('Please fill all fields');
-//         return false;
-//     }
-
-
-//     if (!/^\d+$/.test(PHONEValue)) {
-//         alert('Phone number can only contain numbers');
-//         return false;
-//     }
-
-
-//     if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(eMAILValue)) {
-//         alert('Please enter a valid email address');
-//         return false;
-//     }
-
-//     return true;
-// }
-
-
 function btnClick(){
     let btn;
 
     var name=document.getElementById("name").value.trim();
-    var eMAIL=document.getElementById("email").value.trim();
+    var eMail=document.getElementById("email").value.trim();
     var num=document.getElementById("mobile").value.trim();
     var msg=document.getElementById("message").value.trim();
 
 
-    if(name==="" || eMAIL==="" || num==="" || msg==="")
+    if(name==="" || eMail==="" || num==="" || msg==="")
     {
         alert('Please fill all fields');
         btn=false;
-    }else if(!/^([a-zA-Z0-9\._]+)@([a-zA-Z0-9]+).([a-z]+).([a-z]+)?$/.test(eMAIL)){
+    }else if(!/^([a-zA-Z0-9\._]+)@([a-zA-Z0-9]+).([a-z]+).([a-z]+)?$/.test(eMail)){
         alert('Please enter a valid Email adress');
 
     
